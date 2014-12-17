aed838327f4c3d91ea4ea8e34be3cb444ca8b031
Heloisa <heloisafspagnoli@gmail.com>
2014-12-17 12:30:45 -0200

    Adding media files

--- movbrasil/dados_basicos/models.py
+++ movbrasil/dados_basicos/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
+from django.conf import settings
 
 class quem_somos_model(models.Model):
     descricao = models.TextField()
@@ -9,7 +10,7 @@ class time_mvb_model(models.Model):
     nome = models.CharField(max_length=200)
     tel = models.CharField(max_length=11) 
     e_mail= models.EmailField()
-    foto = models.ImageField(upload_to = 'pic_folder/', blank=True, null=True)
+    foto = models.ImageField(blank=True, null=True)
 
     def __unicode__(self):
         return self.nome
--- movbrasil/dados_basicos/views.py
+++ movbrasil/dados_basicos/views.py
@@ -4,10 +4,8 @@ from .models import quem_somos_model, time_mvb_model
 
 def quem_somos_view(request):
     descricao = quem_somos_model.objects.first().descricao
-    return render(request, 'dados_basicos/quem_somos.html',
-                    {'descricao': descricao})
+    return render(request, 'quem_somos.html', {'descricao': descricao})
 
 def time_mvb_view(request):
     time = time_mvb_model.objects.all()
-    return render(request, 'dados_basicos/time_mvb.html',
-                    {'time_mvb': time})
+    return render(request, 'time_mvb.html', {'time_mvb': time})
