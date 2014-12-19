7ae8362ef4cee4fcf1066664201117d1adc91d00
Heloisa <heloisafspagnoli@gmail.com>
2014-12-19 21:49:53 -0200

    Create base html and others templates

--- movbrasil/dados_basicos/views.py
+++ movbrasil/dados_basicos/views.py
@@ -6,8 +6,7 @@ from .models import quem_somos_model, equipe_model
 
 def quem_somos_view(request):
     descricao = quem_somos_model.objects.first().descricao
-    return render(request, 'quem_somos.html', {'descricao': descricao})
-
-def equipe_view(request):
     time_mvb = equipe_model.objects.all()
-    return render(request, 'time_mvb.html', {'time_mvb': time_mvb})
+    return render(request, 'quem_somos.html', 
+                    {'descricao': descricao, 'time_mvb': time_mvb})
+
