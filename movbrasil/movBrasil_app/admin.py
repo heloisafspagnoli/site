261988494faff3b5a2915a2e5e06bf2ecdd4702b
Heloisa <heloisafspagnoli@gmail.com>
2014-12-26 15:19:58 -0200

    Pagination working

--- movbrasil/movBrasil_app/admin.py
+++ movbrasil/movBrasil_app/admin.py
@@ -2,12 +2,16 @@ from django.contrib import admin
 
 from models import chamada_model, parceiros_model, quem_somos_model, equipe_model, endereco_contato_model, local_model, curso_model, palestrantes_model, programacao_model
 
+class CursoAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("titulo",)}
+
+
 admin.site.register(chamada_model)
 admin.site.register(quem_somos_model)
 admin.site.register(equipe_model)
 admin.site.register(parceiros_model)
 admin.site.register(endereco_contato_model)
 admin.site.register(local_model)
-admin.site.register(curso_model)
+admin.site.register(curso_model, CursoAdmin)
 admin.site.register(palestrantes_model)
 admin.site.register(programacao_model)
