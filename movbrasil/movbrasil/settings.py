14cd38017875aa795d9caa6f962133af24563b83
Heloisa <heloisafspagnoli@gmail.com>
2015-04-08 15:40:29 -0300

    Update

--- movbrasil/movbrasil/settings.py
+++ movbrasil/movbrasil/settings.py
@@ -101,8 +101,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_SSL = True
 #EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'heloisafspagnoli@gmail.com'
-EMAIL_HOST_PASSWORD = 'fdebrx14ns;)'
+EMAIL_HOST_USER = 'diego@movbrasil.com.br'
+EMAIL_HOST_PASSWORD = 'movbrasil321'
 #EMAIL_PORT = 587
 EMAIL_PORT = 465
 
@@ -116,4 +116,4 @@ STATICFILES_DIRS = (
                                                                                
 # Media files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')               
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
